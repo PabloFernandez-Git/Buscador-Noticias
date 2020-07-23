0ba94aa6c7b474aec834baf8950265647a1bdf92
Pablo Fernandez <pablo.ezequiel.fernandez@gmail.com>
2020-07-23 13:00:54 -0300

    Formulario y Módulos CSS

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react';
 import Header from './components/Header';
+import Formulario from './components/Formulario';
 
 
 function App() {
@@ -10,7 +11,9 @@ function App() {
         />
 
         <div className="container white">
-          <h1>Formulario</h1>
+          <Formulario 
+            
+          />
         </div>
     </Fragment>
   );
